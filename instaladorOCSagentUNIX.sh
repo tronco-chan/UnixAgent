3c9fe71b7e91a49920519a64020c183d083f039e
tronco-chan <73654249+tronco-chan@users.noreply.github.com>
2020-11-19 12:45:13 +0100

    Update instaladorOCSagentUNIX.sh

--- instaladorOCSagentUNIX.sh
+++ instaladorOCSagentUNIX.sh
@@ -36,7 +36,7 @@ function ejecucionINSTALL() {
   #wget https://github.com/tronco-chan/UnixAgent/blob/master/UnixAgent-master.tar.gz
   tar -xvzf UnixAgent-master.tar.gz
   cd UnixAgent-master
-  env PERL_AUTOINSTALL=1 perl Makefile.PL && make && make install && perl postinst.pl --nowizard --server=https://ocsng.altia.es/ocsinventory --crontab --nossl
+  sudo env PERL_AUTOINSTALL=1 perl Makefile.PL && make && make install && perl postinst.pl --nowizard --server=https://ocsng.altia.es/ocsreports/ --crontab --nossl
   #make
   #make install
 }
